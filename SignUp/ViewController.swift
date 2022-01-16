6268e63f5c4fd26f8021661797ffd65f38f2ee47
mikhail <Morochko2014@gmail.com>
2022-01-16 21:29:20 +0300

    Add UI

--- SignUp/ViewController.swift
+++ SignUp/ViewController.swift
@@ -1,6 +1,32 @@
 import UIKit
 
-final class ViewController: UIViewController {
+final class ViewController: UITableViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak private var emailAddressTextField: UITextField!
+    @IBOutlet weak private var passwordTextField: UITextField!
+    @IBOutlet weak private var passwordConfirmationTextField: UITextField!
+    @IBOutlet weak private var agreeTermsSwitch: UISwitch!
+    @IBOutlet weak private var signUpButton: BigButton!
+
+    // MARK: - Actions
+
+    @IBAction private func emailDidChange(_ sender: Any) { }
+
+    @IBAction private func passwordDidChange(_ sender: Any) { }
+
+    @IBAction private func passwordConfirmationDidChange(_ sender: Any) { }
+
+    @IBAction private func agreeSwitchDidChange(_ sender: Any) { }
+
+    @IBAction private func signUpTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Welcome!", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
