4b154bd9430b81ab88e9fe6a1870d26edc162eaa
mikhail <Morochko2014@gmail.com>
2022-01-16 21:04:54 +0300

    Cleaning

--- SignUp/ViewController.swift
+++ SignUp/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  SignUp
-//
-//  Created by Mikhail Morochko on 16.01.22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
